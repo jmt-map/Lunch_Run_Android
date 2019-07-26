04a70dc7a5e9a092a08e92d9a8ed7bd2117bacfa
ina-uzu <ina.uzu.song@gmail.com>
2019-07-26 09:24:07 +0900

    로그인 레이아수 수정 & bottom tab nav 추가

---
+++ app/src/main/java/com/inauzu/lunchrun/review/ReviewFragment.java
@@ -0,0 +1,19 @@
+package com.inauzu.lunchrun.review;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.inauzu.lunchrun.R;
+
+public class ReviewFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_review_menu, container, false);
+    }
+}
