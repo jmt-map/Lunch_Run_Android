44e377e118bd634cb600704a21baf3fcaff45747
ina-uzu <ina.uzu.song@gmail.com>
2019-08-04 23:01:21 +0900

    old ver

--- app/src/main/java/com/inauzu/lunchrun/login/LoginActivity.java
+++ app/src/main/java/com/inauzu/lunchrun/login/LoginActivity.java
@@ -12,9 +12,13 @@ import android.widget.Toast;
 import com.inauzu.lunchrun.MainActivity;
 import com.inauzu.lunchrun.Temp;
 import com.inauzu.lunchrun.R;
+import com.inauzu.lunchrun.model.Review;
+import com.inauzu.lunchrun.model.User;
 import com.inauzu.lunchrun.model.testModel;
 import com.inauzu.lunchrun.retrofit.ApiClient;
 import com.inauzu.lunchrun.retrofit.ApiInterFace;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -40,31 +44,46 @@ public class LoginActivity extends Activity {
                 //startActivity(intent);
 
                 ApiInterFace apiService = ApiClient.getCliten().create(ApiInterFace.class);
-                Call<testModel> call = apiService.getTestModel();
-                ((Call) call).enqueue(new Callback<testModel>() {
+
+                Call<testModel> call3 = apiService.postTestModel("Tiana", "1234567", etId.getText().toString(), etPwd.getText().toString());
+                call3.enqueue(new Callback<testModel>() {
                     @Override
                     public void onResponse(Call<testModel> call, Response<testModel> response) {
-                        testModel test = response.body();
-                        Log.d("OK", "hoho " + test.getToken() + " CODE =" + response.code());
+                        Log.d("POST OK", "CODE =" + String.valueOf(response.code()));
                     }
 
                     @Override
                     public void onFailure(Call<testModel> call, Throwable t) {
-                        Toast.makeText(getApplicationContext(), "Error",Toast.LENGTH_LONG);
+                        Log.e("ERROR", t.toString());
                     }
                 });
 
-                Call<testModel> call2 = apiService.getTestModel2();
-                call2.enqueue(new Callback<testModel>() {
+                Call<List<Integer>> call4 = apiService.getNumbers();
+                call4.enqueue(new Callback<List<Integer>>() {
                     @Override
-                    public void onResponse(Call<testModel> call, Response<testModel> response) {
-                        Toast.makeText(getApplicationContext(), "OK",Toast.LENGTH_LONG);
-                        testModel test = response.body();
-                        Log.d("OK", "hoho2  CODE =" + response.code());
+                    public void onResponse(Call<List<Integer>> call, Response<List<Integer>> response) {
+                        List<Integer> list = response.body();
+                        Log.d("POST OK", list.toString() + " CODE =" + String.valueOf(response.code()));
                     }
 
                     @Override
-                    public void onFailure(Call<testModel> call, Throwable t) {
+                    public void onFailure(Call<List<Integer>> call, Throwable t) {
+                        Log.e("ERROR_ARRina@", t.toString());
+                    }
+                });
+
+                Integer id = 1;
+                Call<Review> call5 = apiService.getReview(id);
+                call5.enqueue(new Callback<Review>() {
+                    @Override
+                    public void onResponse(Call<Review> call, Response<Review> response) {
+                        Review review = response.body();
+                        Log.d("GET OK", review.toString() + " CODE " + response.code());
+                    }
+
+                    @Override
+                    public void onFailure(Call<Review> call, Throwable t) {
+                        Log.e("ERROR_GET", t.toString());
 
                     }
                 });
--- app/src/main/java/com/inauzu/lunchrun/retrofit/ApiInterFace.java
+++ app/src/main/java/com/inauzu/lunchrun/retrofit/ApiInterFace.java
@@ -1,12 +1,18 @@
 package com.inauzu.lunchrun.retrofit;
 
+import com.inauzu.lunchrun.model.Review;
+import com.inauzu.lunchrun.model.User;
 import com.inauzu.lunchrun.model.testModel;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 public interface ApiInterFace {
     @GET("test")
@@ -16,8 +22,18 @@ public interface ApiInterFace {
     Call<testModel> getTestModel2();
 
     @FormUrlEncoded
-    @POST("test3")
+    @POST("post")
     Call<testModel> postTestModel(
-        @Field("id") int id
+        @Field("alias") String alias,
+        @Field("phone") String phone,
+        @Field("email") String email,
+        @Field("password") String password
     );
+
+    @GET("test3")
+    Call<List<Integer>> getNumbers();
+
+    @Headers({"Content-Type:application/x-www-form-urlencoded"})
+    @GET("test4")
+    Call<Review> getReview(@Query("restaurant_id") Integer restaurant_id);
 }
